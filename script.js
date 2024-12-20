ebf6b5f21182abe9ebcc5294d1fc3917b02360f7
Hércules Machado <herculesprogramador911@gmail.com>
2024-12-20 08:37:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function apiCard() {
         return response.json(); // Converte para um objeto JavaScript
     })
     .then(data => {
+        // div feita no html como referencia
         const gallery = document.getElementById('container-cards');
     
         // Itera sobre as categorias no JSON
         Object.keys(data).forEach(categoria => {
+            // cria o container da categoria
             const categoryContainer = document.createElement('div');
             categoryContainer.classList.add('categoria');
     
@@ -27,26 +29,32 @@ function apiCard() {
     
             // Adiciona as imagens e descrições da categoria
             data[categoria].forEach(item => {
+                // cria o card
                 const div = document.createElement('div');
                 div.classList.add('card');
-    
+
+                // cria o link para a img
                 const img = document.createElement('img');
                 img.src = `${item.src}`; // Caminho relativo para as imagens
                 img.alt = item.nome;
                 img.classList.add('img-prod')
-    
+
+                // cria a tag p para o nome
                 const nome = document.createElement('p');
                 nome.textContent = item.nome;
                 nome.classList.add('nome-prod');
-    
+
+                // cria a tag p para o valor
                 const valor = document.createElement('p');
                 valor.textContent = `R$${item.valor},00`;
                 valor.classList.add('valor-prod');
     
-    
+                // Adiciona cada div em sus respectivas posições dentro do card
                 div.appendChild(img);
                 div.appendChild(nome);
                 div.appendChild(valor);
+
+                // card final
                 imageContainer.appendChild(div);
             });
     
