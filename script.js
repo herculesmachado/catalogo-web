7f4fddfd182b9916cc5ac641d963395f3954ccf5
Hércules Machado <herculesprogramador911@gmail.com>
2025-03-02 17:55:00 -0300

    feature do codigo

--- script.js
+++ script.js
@@ -1,71 +1,57 @@
 function apiCard() {
-
-    // Carregar o arquivo JSON e renderizar os dados no HTML
-    fetch('./server.json') // Nome do arquivo JSON
+    fetch('./server.json')
     .then(response => {
         if (!response.ok) {
             throw new Error('Erro ao carregar o JSON.');
         }
-        return response.json(); // Converte para um objeto JavaScript
+        return response.json();
     })
     .then(data => {
-        // div feita no html como referencia
         const gallery = document.getElementById('container-cards');
     
-        // Itera sobre as categorias no JSON
         Object.keys(data).forEach(categoria => {
-            // cria o container da categoria
             const categoryContainer = document.createElement('div');
             categoryContainer.classList.add('categoria');
     
-            // Cria o título da categoria
             const title = document.createElement('h2');
             title.textContent = categoria;
             title.classList.add('categoria-titulo');
     
-            // Contêiner para as imagens
             const imageContainer = document.createElement('div');
             imageContainer.classList.add('cards-img');
     
-            // Adiciona as imagens e descrições da categoria
+            // Ordena os itens do menor para o maior valor
+            data[categoria].sort((a, b) => a.valor - b.valor);
+    
             data[categoria].forEach(item => {
-                // cria o card
                 const div = document.createElement('div');
                 div.classList.add('card');
 
-                // cria o link para a img
                 const img = document.createElement('img');
-                img.src = `${item.src}`; // Caminho relativo para as imagens
+                img.src = `${item.src}`;
                 img.alt = item.nome;
-                img.classList.add('img-prod')
+                img.classList.add('img-prod');
 
-                // cria a tag p para o nome
                 const nome = document.createElement('p');
                 nome.textContent = item.nome;
                 nome.classList.add('nome-prod');
 
-                // cria a tag p para o valor
                 const valor = document.createElement('p');
                 valor.textContent = `R$${item.valor},00`;
                 valor.classList.add('valor-prod');
 
-
-                // cria a tag p para descrição
-                const desc = document.createElement('p')
+                const desc = document.createElement('p');
                 desc.textContent = item.descricao;
                 desc.classList.add('desc-prod');
     
-                // Adiciona cada div em sus respectivas posições dentro do card
                 div.appendChild(img);
                 div.appendChild(nome);
                 div.appendChild(desc);
                 div.appendChild(valor);
 
-                // card final
                 imageContainer.appendChild(div);
             });
     
-            // Adiciona o título e as imagens ao contêiner da categoria
             categoryContainer.appendChild(title);
             categoryContainer.appendChild(imageContainer);
             gallery.appendChild(categoryContainer);
@@ -74,5 +60,4 @@ function apiCard() {
     .catch(error => console.error('Erro ao carregar o JSON:', error));
 }
 
-apiCard()
-
+apiCard();
